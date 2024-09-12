823f7bcb7937a31fdf7d1343b86f7c2427bde5cd
Kymorh <pupil5a5_2020@gymnasium283.kyiv.ua>
2024-09-12 21:34:35 +0300

    start app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+let pred=["o","i"]
+
+  return (
+   <div className='r'>
+    <img onClick={()=>console.log(Math.floor( Math.random()*100) )
+    } src="./shar.PNG" alt="" />
+    <span>да</span>
+   </div>
+
+  )
+}
+
+export default App
