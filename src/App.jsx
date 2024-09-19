af8d36b5a34aa0104d8a6da2a2084070365c9564
Kymorh <pupil5a5_2020@gymnasium283.kyiv.ua>
2024-09-19 19:18:59 +0300

    finised prediction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+
+import { useState } from 'react'
 import './App.css'
 
-function App() {
-let pred=["o","i"]
+function App(){
 
+  
+  const predictions = [
+    "Скоро встанешь... с дивана.",
+    "Код заработает с первого раза. Или нет.",
+    "Тебе светит отпуск... через 10 лет.",
+    "Сегодня твой кофе будет особенно горьким.",
+    "Wi-Fi внезапно улучшится на секунду.",
+    "Жди внезапного желания ничего не делать.",
+    "Удача улыбается тебе... но с сарказмом.",
+    "Сегодня ты точно забудешь, зачем открыл вкладку.",
+    "Завтра пятница! Но не сегодня.",
+    "Чувствуй себя круто, даже если никто не видит.",
+    "Грядут великие дела... но не с тобой.",
+    "Ты уже почти гений... почти.",
+    "Сегодня удачный день для ничего.",
+    "Скоро ты найдешь... потерянный носок.",
+    "Не забудь поесть, пока код компилируется.",
+    "Твоя удача — это баг, который стал фичей.","Да","Нет"
+  ];
+const [prediction, setPrediction] = useState(predictions[Math.floor(Math.random()*predictions.length)])
   return (
    <div className='r'>
-    <img onClick={()=>console.log(Math.floor( Math.random()*100) )
-    } src="./shar.PNG" alt="" />
-    <span>да</span>
+    <img  src="./shar.PNG" alt="" />
+
+  <div className='text'onClick={()=> setPrediction(predictions[Math.floor(Math.random()*predictions.length)]) }>{prediction}</div>
    </div>
 
   )
